0c5c0f14548b2065f0dc2ecb5705f59e73c808d9
louisep1 <89385533+louisep1@users.noreply.github.com>
2022-08-18 15:22:03 +0900

    fixing styles and building reservation page

--- frontend/src/pages/Reservation.jsx
+++ frontend/src/pages/Reservation.jsx
@@ -6,7 +6,76 @@ const Reservation = () => {
     <div>
       <Banner title='Reservation' />
 
-      <div className="section"></div>
+      <div className="section">
+        <div className="form-container p-4">
+          <p className='text-md'>Book your stay</p>
+          <form className='reservation-form'>
+
+            {/* !!! CHANGE VALUE TO STATE */}
+            <div>
+              <div className="input-group">
+                <label htmlFor="hotel-location">Location</label>
+                <select name="" id="hotel-location">
+                  <option value="tokyo">Tokyo</option>
+                </select>
+              </div>
+
+
+              <div className="input-group">
+                <label htmlFor="">Check-in date</label>
+                <input type="date" />
+              </div>
+            </div>
+
+            <div>
+              <div className="input-group">
+                <label htmlFor="">Nights</label>
+                <select name="" id="">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+
+              <div className="input-group">
+                <label htmlFor="total-guests">Number of guests (adults)</label>
+                <select name="" id="total-guests">
+                  {/* !!! maybe make this dynamic so only includes 2 per room, then suggest children are not included  */}
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+
+            </div>
+
+
+
+            <div className="input-group">
+              <label htmlFor="total-rooms">Number of rooms</label>
+              <select name="" id="total-rooms">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+            </div>
+
+
+            {/* display room type on next search result page */}
+
+          </form>
+        </div>
+
+      </div>
     </div>
   )
 }
