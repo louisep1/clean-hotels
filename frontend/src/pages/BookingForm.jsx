c18c3bbfa106588cafef83e1a24749d05d47ec73
louisep1 <89385533+louisep1@users.noreply.github.com>
2022-08-31 14:58:38 +0900

    booking form page

--- frontend/src/pages/BookingForm.jsx
+++ frontend/src/pages/BookingForm.jsx
@@ -1,8 +1,8 @@
-// fix styling for the room row on this page
-// add other image options on the page and reservation page for other room types
 // add the customers information input section on this page
 // submit button
-// actually requests to backend for reserving room - take dates off of available for that room then produce a second booking item in booking table
+// send put request to backend x 2
+//     => update availability table to be false (not available) for selected room and dates
+//     => add a new reservation to the reservations table (to be created) 
 
 
 import { useEffect } from 'react'
@@ -38,15 +38,12 @@ const BookingForm = () => {
         <div className='results-container'>
           <div className='title py-4'>Booking details</div>
 
-          {/* if searching for 2 people and one room, filter out the single room option: */}
-          {/* !!! need to go back and adjust this for more than 2 people and more than 1 room options */}
+          {/* !!! BACK BUTTON */}
 
           <div className="result" key={result.id}>
             <img src={result.type === 'single' ? singleRoom : doubleRoom} alt="room" className='img' />
             <p className='room'>{result.type === 'single' ? 'Single ' : 'Double '} Room</p>
             <p className='cost'>${result.rate}</p>
-            {/* !!! the cost is just for one room - currently unable to book two rooms (and therefore can't book for more than 2 people either) */}
-
             <p className='details'>{search.guests} <BsFillPersonFill className='icon' /> / {search.rooms} <MdNightlight className='icon' /></p>
             <div className="link"></div>
             <div className="reserve"></div>
@@ -55,9 +52,11 @@ const BookingForm = () => {
           </div>
         </div>)}
 
-      <form className='m-4'>
-        <label htmlFor="">Email address:</label>
-        <input type="text" />
+      <form className='booking'>
+        <div className="email">
+          <label htmlFor="">Email address:</label>
+          <input type="text" />
+        </div>
         <button>Submit booking</button>
       </form>
     </div>
